97e00ca6ade366bf3d414bed71f26e1ec8d5f1e4
Bohusla <124197054+Bohusla@users.noreply.github.com>
2023-03-13 22:05:50 +0100

    Update vollscript.sh

--- vollscript.sh
+++ vollscript.sh
@@ -6,18 +6,19 @@ echo "Hello Student!"
 echo -n "Enter your name: "
 read name
 echo "Wellcome to terminal $name
-mkdir -p /tmt/test
+mkdir -p /tmp/test
 touch /tmp/test/mydate.txt
 date +"%H%M%S"
 for run in {1..10}
-do echo $run
-sleep 1
+do 
+echo $run
+sleep .5
 done
 echo "Data saved. Continue work"
 df -h >> /tmp/test/mydate.txt
 mkdir -p /opt/mydate
 cp /tmp/test/mydate.txt /opt/mydate/newmydate.txt
-fur run in {1..5}
+for run in {1..5}
 do echo $run
 sleep 1
 done
@@ -28,3 +29,6 @@ chmod 744 /bin/fullscript.sh
 ./fullscript.sh
 export FULLSCRIPT=/bin/fullscript.sh
 $FULLSCRIPT
+
+#
+fullscript.sh #запустится с любого места. т.к. в папке bin
