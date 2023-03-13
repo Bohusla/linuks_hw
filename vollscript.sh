5112ebe95b91fce8770b0e8399d33f55f7310174
Bohusla <124197054+Bohusla@users.noreply.github.com>
2023-03-13 15:37:51 +0100

    Create vollscript.sh

---
+++ vollscript.sh
@@ -0,0 +1,30 @@
+
+nano /bin/fullscript.sh
+
+#!/bin/bash
+echo "Hello Student!"
+echo -n "Enter your name: "
+read name
+echo "Wellcome to terminal $name
+mkdir -p /tmt/test
+touch /tmp/test/mydate.txt
+date +"%H%M%S"
+for run in {1..10}
+do echo $run
+sleep 1
+done
+echo "Data saved. Continue work"
+df -h >> /tmp/test/mydate.txt
+mkdir -p /opt/mydate
+cp /tmp/test/mydate.txt /opt/mydate/newmydate.txt
+fur run in {1..5}
+do echo $run
+sleep 1
+done
+echo "Well done Boss"
+
+#
+chmod 744 /bin/fullscript.sh
+./fullscript.sh
+export FULLSCRIPT=/bin/fullscript.sh
+$FULLSCRIPT
